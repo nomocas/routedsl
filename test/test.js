9814adbbcb673234caf1d57d2acace2ad397c3e8
Gilles Coomans <gilles.coomans@gmail.com>
2016-04-01 01:46:33 +0200

    add more tests

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
  *
  */
 if (typeof require !== 'undefined')
-	var chai = require("chai"),
+	var chai = require("./chai"),
 		routedsl = require("../index"),
 		v = routedsl.v;
 else
@@ -22,17 +22,83 @@ describe("local", function() {
 
 	it("should", function() {
 		expect(desc.index).to.equals(2)
-		expect(desc.output).to.deep.equals({
+		expect(desc.params).to.deep.equals({
 			bloupi: 'bloupi',
 			lollipop: 'lollipop'
 		});
 		expect(desc2.index).to.equals(4)
-		expect(desc2.output).to.deep.equals({
+		expect(desc2.params).to.deep.equals({
 			foo: 'foo',
 			bar: 'bar'
 		});
 	});
 });
+
+describe("not match ", function() {
+	var url = '/';
+	var route = new routedsl('/foo');
+	var desc = route.match(url);
+
+	it("should", function() {
+		expect(desc).to.equals(false);
+	});
+});
+
+describe("match key", function() {
+	var url = '/foo';
+	var route = new routedsl('/foo');
+	var desc = route.match(url);
+
+	it("should", function() {
+		expect(desc).to.not.equals(false);
+		expect(desc.index).to.equals(1);
+		expect(desc.params).to.deep.equals({});
+	});
+});
+
+
+describe("not match key", function() {
+	var url = '/!foo';
+	var route = new routedsl('/foo');
+	var desc = route.match(url);
+
+	it("should", function() {
+		expect(desc).to.equals(false);
+	});
+});
+
+describe("match optional key", function() {
+	var url = '/bar';
+	var route = new routedsl('/?bar');
+	var desc = route.match(url);
+
+	it("should", function() {
+		expect(desc).to.not.equals(false);
+		expect(desc.index).to.equals(1);
+		expect(desc.params).to.deep.equals({});
+	});
+});
+
+describe("not match with optional key", function() {
+	var url = '/foo';
+	var route = new routedsl('/?bar');
+	var desc = route.match(url);
+
+	it("should", function() {
+		expect(desc).to.equals(false);
+	});
+});
+
+describe(" match with or", function() {
+	var url = '/bar';
+	var route = new routedsl('/[bar,$]');
+	var desc = route.match('/bar') && route.match('/');
+
+	it("should", function() {
+		expect(desc).to.not.equals(false);
+	});
+});
+
 
 describe("end at root", function() {
 	var url = '/';
@@ -42,6 +108,19 @@ describe("end at root", function() {
 	it("should", function() {
 		expect(desc).to.not.equals(false);
 		expect(desc.index).to.equals(0);
-		expect(desc.output).to.deep.equals({});
+		expect(desc.params).to.deep.equals({});
+	});
+});
+
+
+describe("root match and route has more parts", function() {
+	var url = '/foo';
+	var route = new routedsl('/');
+	var desc = route.match(url);
+
+	it("should", function() {
+		expect(desc).to.not.equals(false);
+		expect(desc.index).to.equals(0);
+		expect(desc.params).to.deep.equals({});
 	});
 });
